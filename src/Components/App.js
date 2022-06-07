a8e58f86af48f45c9db4a290bd914b19fa6fb82f
Nicholas Ao <aominhlong@yahoo.com>
2022-06-07 13:24:32 -0600

    Add App skeleton and create Component and Style folders

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import '../Styling/App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="App">
+          
+      </div>
+    );
+  }
+}
+
+export default App;
