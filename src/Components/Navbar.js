0cb42df4d2f1e9b866b50c550ed0eab9fbb618a1
Nicholas Ao <aominhlong@yahoo.com>
2022-06-09 10:32:11 -0600

    Place in functionality for onClick to show a specific genre

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,22 +2,22 @@ import React from 'react';
 import '../Styling/Navbar.css'
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ chooseGenre, clearSearchedAnime }) {
     return(
         <div className='navbar'>
             <Link to='/'>
-                <button className='home-btn'>Home</button>
+                <button className='home-btn' onClick={() => clearSearchedAnime()}>Home</button>
             </Link>
             <div className='dropdown'>
                     <button className='categories'>Genre</button>
                     <div className='dropdown-content'>
-                        <a href='#' id='Action'>Action</a>
-                        <a href='#' id='Comedy'>Comedy</a>
-                        <a href='#' id='Adventure'>Adventure</a>
-                        <a href='#' id='Drama'>Drama</a>
-                        <a href='#' id='Sport'>Sport</a>
-                        <a href='#' id='Fantasy'>Fantasy</a>
-                        <a href='#' id='Sci-Fi'>Sci-Fi</a>
+                        <a href='#' id='Action' onClick={(event => chooseGenre(event))}>Action</a>
+                        <a href='#' id='Comedy' onClick={(event => chooseGenre(event))}>Comedy</a>
+                        <a href='#' id='Adventure' onClick={(event => chooseGenre(event))}>Adventure</a>
+                        <a href='#' id='Drama' onClick={(event => chooseGenre(event))}>Drama</a>
+                        <a href='#' id='Sport' onClick={(event => chooseGenre(event))}>Sport</a>
+                        <a href='#' id='Fantasy' onClick={(event => chooseGenre(event))}>Fantasy</a>
+                        <a href='#' id='Sci-Fi' onClick={(event => chooseGenre(event))}>Sci-Fi</a>
                     </div>
                 </div>
         </div>
