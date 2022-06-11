c166664b2806dec8a8052a4d0500b30e1037bfd3
Nicholas Ao <aominhlong@yahoo.com>
2022-06-11 10:41:08 -0600

    Add deployment links

--- src/Components/App.js
+++ src/Components/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/api/v1/anime')
+    fetch('https://anime-api-showcase.herokuapp.com/api/v1/anime')
     .then(res => res.json())
     .then(data => this.setState({ allAnime: data.animeList, myWatchList: data.userWatchList, randomAnime: data.animeList[this.getRandomIntInclusive(0, data.animeList.length)] }))
   }
@@ -64,7 +64,7 @@ class App extends Component {
     if (this.state.myWatchList.length < 1 && !this.state.myWatchList.includes(newAnime)) {
       this.setState({ myWatchList: [newAnime] })
 
-      fetch(`http://localhost:3001/api/v1/anime`, {
+      fetch(`https://anime-api-showcase.herokuapp.com/api/v1/anime`, {
       method: 'POST',
       body: JSON.stringify({
         "title": newAnime.title,
@@ -81,7 +81,7 @@ class App extends Component {
     } else if (!this.state.myWatchList.includes(newAnime)) {
       this.setState({ myWatchList: [...this.state.myWatchList, newAnime] })
 
-      fetch(`http://localhost:3001/api/v1/anime`, {
+      fetch(`https://anime-api-showcase.herokuapp.com/api/v1/anime`, {
       method: 'POST',
       body: JSON.stringify({
         "title": newAnime.title,
@@ -102,7 +102,7 @@ class App extends Component {
       return anime.title.toLowerCase() === title.toLowerCase()
     })
     console.log(title)
-    fetch(`http://localhost:3001/api/v1/anime`, {
+    fetch(`https://anime-api-showcase.herokuapp.com/api/v1/anime`, {
       method: 'DELETE',
       body: JSON.stringify({
         "title": foundAnime.title,
@@ -119,7 +119,7 @@ class App extends Component {
         console.log(err)
       })
       .then(data => {
-        fetch('http://localhost:3001/api/v1/anime')
+        fetch('https://anime-api-showcase.herokuapp.com/api/v1/anime')
       .then(res => res.json())
       .then(data => this.setState({ myWatchList: data.userWatchList }))
       })
