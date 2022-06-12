87b449a723298a9e804c02a10c7d2435c9757392
Nicholas Ao <aominhlong@yahoo.com>
2022-06-12 16:21:43 -0600

    Add styling to RandomAnime component

--- src/Components/RandomAnime.js
+++ src/Components/RandomAnime.js
@@ -9,15 +9,17 @@ function RandomAnime({ randomAnime, addToWatchList, myWatchList }) {
             return anime.title === randomAnime.title })
         return(
             <div className='random-anime'>
+                <div className='random-anime-container'>
                 <h1 className='pick-of-the-day'>Anime Pick of the Day</h1>
                 <img className='random-anime-img' src={ randomAnime.image } alt={'Poster of ' + randomAnime.title }></img>
                 
-                { foundAnime.length ? <h5>Anime is in your watch list</h5> : 
+                { foundAnime.length ? <h5>(Anime is in your watch list)</h5> : 
                 <button className='add-random-anime' onClick={(() => addToWatchList(randomAnime.title))}>Add Anime to Watchlist</button> }
 
                 <h1 className='random-anime-title'>{ randomAnime.title }</h1>
                 <p className='random-anime-synopsys'>{ randomAnime.synopsis }</p>
                 <p className='random-anime-genre'>({ randomAnime.genre.join(', ') })</p>
+                </div>
             </div>
         )
     } 
