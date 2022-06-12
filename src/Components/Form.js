a616b7f030e3208881cd81fae097902f139e798f
Nicholas Ao <aominhlong@yahoo.com>
2022-06-12 16:21:24 -0600

    Add styling to Form component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../Styling/Form.css';
+import moon from '../Images/moon.png'
 
 function Form({ input, searchAnime, clearSearchedAnime }) {
     return (
         <div>
             <div className='header'>
-                 <h1 className='app-title'>Ani-Planet</h1>
-                 <Link to='/'>
-                    <button className='home-btn' onClick={() => clearSearchedAnime()}>Home</button>
-                 </Link>
-                 <input className='search-bar' type='text' placeholder='search' value={input} onChange={(event => searchAnime(event))}></input>
+                <Link to='/'>
+                <div className='app-title-container'>
+                <img class='app-img' src={ moon } alt='moon picture'></img>
+                    <h1 className='app-title'>Ani-Planet</h1>
+                </div>
+                </Link>
+                <div className='search-home-container'>
+                    <Link to='/'>
+                        <button className='home-btn' onClick={() => clearSearchedAnime()}>Home</button>
+                    </Link>
+                    <input className='search-bar' type='text' placeholder='search' value={input} onChange={(event => searchAnime(event))}></input>
+                </div>
             </div>
         </div>
     )
